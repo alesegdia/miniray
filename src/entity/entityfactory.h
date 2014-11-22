c98d8baae9c2d7dd90f9321d87cf603c78f251c5
Alejandro Seguí <alesegdia@gmail.com>
2014-11-22 16:15:07 +0100

    Player spawning at EntityFactory.

--- src/entity/entityfactory.h
+++ src/entity/entityfactory.h
@@ -24,6 +24,7 @@ public:
 			DynamicArray<Entity*>* bulletlist
 		);
 
+	Player* SpawnPlayer( float x, float y );
 	void SpawnBullet( const cml::vector2f& pos, const cml::vector2f& dir, CollisionLayer col, uint16_t mask, Sprite3D* sprite, float time );
 	void SpawnPlayerBullet( cml::vector2f pos, cml::vector2f dir, float time );
 	void SpawnEnemyBullet( const cml::vector2f& pos, const cml::vector2f& dir, float time );
