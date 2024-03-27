9766b8f00a3f5cd85ac5e41b0f26a6a1edff4a07
Alejandro Seguí <alejandro.segui@thebreachstudios.com>
2024-03-28 00:49:13 +0100

    werks

--- src/game/entity/controller/mobaicontroller.h
+++ src/game/entity/controller/mobaicontroller.h
@@ -27,22 +27,60 @@ public:
 		MobAIController::entityfactory = ef;
 	}
 
-	void Step( Entity* e, uint32_t delta )
+	void Step(Entity* e, uint32_t delta)
 	{
 		Mob* mob = static_cast<Mob*>(e);
-        assert(mob != nullptr && "THE OBJECT IS NOT A MOB");
+		assert(mob != nullptr && "THE OBJECT IS NOT A MOB");
 		mob->ClearVelocity();
-		CheckHealth( mob );
-		DoDropItem( mob, rng, entityfactory );
-		DoSensePlayer( mob, player );
+		CheckHealth(mob);
+		DoDropItem(mob, rng, entityfactory);
+		DoSensePlayer(mob, player);
 
-		if( mob->player_visible || mob->painLastFrame)
+		if (mob->player_visible || mob->painLastFrame)
 		{
 			DoKeepDistance(mob, player, delta);
 		}
 		mob->skillSet.SetPressed(mob->player_visible);
 		mob->skillSet.update(delta);
 	}
+
+};
+
+
+
+
+
+class SpawnerAIController : public EntityController
+{
+public:
+	SpawnerAIController(std::shared_ptr<EntityFactory> efactory)
+		: m_efactory(efactory)
+	{
+
+	}
+	void Step(Entity* e, uint32_t delta)
+	{
+		m_timer += delta;
+		if (m_timer >= m_timeBetweenSpawns)
+		{
+			m_timer -= m_timeBetweenSpawns;
+			auto enemy = m_efactory->SpawnEnemy(1, 1);
+			m_numSpawned++;
+			enemy->onDie = [this](Entity* e) {
+				this->m_numSpawned--;
+			};
+		}
+	}
+
+private:
+
+	std::shared_ptr<EntityFactory> m_efactory;
+	int m_numSpawned = 0;
+	int m_capacity = 6;
+	uint32_t m_timeBetweenSpawns = 5000;
+	uint32_t m_timer = 0;
+
+	static RNG rng; // hacerlo puntero y pasarselo desde el main
 
 };
 
